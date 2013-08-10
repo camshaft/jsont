c932c40c742fb1f8078847aa803e64820fc82c4d
Cameron Bytheway <cameron@theflokk.com>
2013-08-09 21:29:46 -0600

    Make the basic template more basic

--- test/cases/basic/index.js
+++ test/cases/basic/index.js
@@ -1,14 +1,4 @@
 
 module.exports = function(jsont) {
-  jsont.pipe('type', function(key, done) {
-    if (key === 'email') return done(null, 'email');
-    return done(null, 'text');
-  });
-
-  jsont.pipe('required', function(key, done) {
-    if (key === 'email') return done(null, true);
-    return done(null, false);
-  });
-
   return jsont;
 };
